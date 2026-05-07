6d797f018e990a8f85bbde23f7d649dbc23566a3
Mustafan4x <Mustafa421670@gmail.com>
2026-05-07 13:09:31 -0500

    feat(data): add TestType, Sex, Hand, MSType enums

---
+++ app/src/test/java/com/mustafan4x/msbattery/data/EnumsTest.kt
@@ -0,0 +1,30 @@
+package com.mustafan4x.msbattery.data
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class EnumsTest {
+
+    @Test
+    fun testTypeHasFiveCases() {
+        assertEquals(5, TestType.values().size)
+    }
+
+    @Test
+    fun testTypeNames() {
+        assertEquals("TAP", TestType.TAP.name)
+        assertEquals("GAIT", TestType.GAIT.name)
+        assertEquals("VISION", TestType.VISION.name)
+        assertEquals("SDMT", TestType.SDMT.name)
+        assertEquals("VOICE", TestType.VOICE.name)
+    }
+
+    @Test
+    fun sexHasFourCases() { assertEquals(4, Sex.values().size) }
+
+    @Test
+    fun handHasThreeCases() { assertEquals(3, Hand.values().size) }
+
+    @Test
+    fun msTypeHasFiveCases() { assertEquals(5, MSType.values().size) }
+}
